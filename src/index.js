e3cf443b01cf9e27f1d669b2cc05b8770bc962e1
Pedro <pedrogdsilva410@gmail.com>
2023-08-03 11:42:50 +0200

    update cardsTable

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const newCardButton = document.getElementById("newCard");
 const messageEl = document.getElementById("message-el");
 const sumEl = document.getElementById("sum-el");
 const cardsEl = document.getElementById("cards-el");
+const tableCardsEl = document.getElementById("tableCards-el")
 const playerEl = document.getElementById("player-el")
 const chipsEl = document.getElementById("chips-el")
 const player = {
@@ -10,9 +11,11 @@ const player = {
 	chips: 0
 };
 let cards = [];
+let tableCards = [];
 let firstCard = 0;
 let secondCard = 0;
 let sum = 0;
+let tableSum = 0;
 let message = "";
 let hasBlackJack = false;
 let isAlive = true;
@@ -31,16 +34,26 @@ function randomCard() {
 }
 
 function startGame() {
+	startGameButton.style.visibility = "hidden";
 	if (!player.playerName) {
 		player.playerName = prompt("What's your name?")
 		player.chips = 100
 
 		playerEl.textContent = "Player: " + player.playerName;
+	}
+
+	if (cards.length < 2) {
+		let bet = prompt('how much do you want to bet (chips: ' + player.chips + ')')
+		player.chips = player.chips - bet;
 		chipsEl.textContent = "Chips: " + player.chips;
 	}
+
 	let firstCard = randomCard();
 	let secondCard = randomCard();
-	cards = [firstCard, secondCard]
+	cards = [firstCard, secondCard];
+	let tableFirstCard = randomCard();
+	let tableSecondCard = randomCard();
+	tableCards = [tableFirstCard, tableSecondCard];
 	let message = "";
 	let hasBlackJack = false;
 	let isAlive = true;
@@ -54,22 +67,28 @@ function startGame() {
 	} else if (sum === 21) {
 		message = "You've got Blackjack!";
 		newCardButton.style.visibility = "hidden";
+		startGameButton.style.visibility = "visible";
+		player.chips = player.chips + bet * 2
 		hasBlackJack = true;
 	} else {
+		startGameButton.style.visibility = "visible";
 		isAlive = false;
 		message = "Bad luck! You're out of the game!";
 	}
 
 	messageEl.textContent = message;
 	cardsEl.textContent = "Cards: " + firstCard + " " + secondCard;
+	tableCardsEl.textContent = "Table Cards: " + tableSecondCard + " " + tableSecondCard;
 	sumEl.textContent = "Sum: " + sum;
 	console.log("blackjack: " + hasBlackJack);
 	console.log("is Alive: " + isAlive);
 }
 
 function drawCard() {
+
 	if (hasBlackJack == false || isAlive == true) {
 		let newCard = randomCard();
+		let newTableCard = randomCard();
 		sum = sum + newCard
 
 		if (sum < 21) {
@@ -86,6 +105,7 @@ function drawCard() {
 
 		messageEl.textContent = message;
 		cardsEl.textContent = cardsEl.textContent + " " + newCard;
+		tableCardsEl.textContent = tableCardsEl.textContent + " " + newTableCard;
 		sumEl.textContent = "Sum: " + sum;
 		console.log("blackjack: " + hasBlackJack);
 		console.log("is Alive: " + isAlive);
